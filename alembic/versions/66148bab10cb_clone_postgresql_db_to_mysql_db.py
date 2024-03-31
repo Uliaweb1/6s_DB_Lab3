d638fbe50f36a6a3993091a3d45a71aab7315bfd
Julia Kolomiets <julia.kolomiets@intela.io>
2024-03-31 20:25:49 +0300

    database migration logic

--- alembic/versions/66148bab10cb_clone_postgresql_db_to_mysql_db.py
+++ alembic/versions/66148bab10cb_clone_postgresql_db_to_mysql_db.py
@@ -29,17 +29,29 @@ def upgrade():
     destination_engine = sa.create_engine(destination_db_uri)
 
     # Query data from source database
-    source_conn = source_engine.connect()
-    source_metadata = sa.MetaData(bind=source_conn)
-    source_table = sa.Table('source_table', source_metadata, autoload=True)
-    source_data = source_conn.execute(source_table.select()).fetchall()
+    # source_conn = source_engine.connect()
+    # source_session = sa.orm.sessionmaker(bind=engine)
+    # source_metadata = sa.MetaData(bind=source_conn)
+    source_metadata = sa.MetaData()
+    source_metadata.reflect(bind=source_engine)
+    # source_table = sa.Table('weather', source_metadata, autoload=True)
+    # source_table = sa.Table('weather', source_metadata)
+    # source_data = source_conn.execute(source_table.select()).fetchall()
 
     # Insert data into destination database
-    destination_conn = destination_engine.connect()
-    destination_metadata = sa.MetaData(bind=destination_conn)
-    destination_table = sa.Table('destination_table', destination_metadata, autoload=True)
-    destination_conn.execute(destination_table.insert(), [dict(row) for row in source_data])
+    # destination_conn = destination_engine.connect()
+    tables_to_migrate = [source_metadata.tables["weather"]]
+    source_metadata.create_all(destination_engine, tables_to_migrate)
+    # destination_table = sa.Table('weather', destination_metadata, autoload=True)
+    # destination_conn.execute(destination_table.insert(), [dict(row) for row in source_data])
 
 
 def downgrade() -> None:
-    pass
+    destination_db_uri = al.context.config.get_main_option('destination_db_uri')
+    destination_engine = sa.create_engine(destination_db_uri)
+    destination_conn = destination_engine.connect()
+    # destination_metadata = sa.MetaData(bind=destination_conn)
+    # destination_table = sa.Table('destination_table', destination_metadata, autoload=True)
+    # destination_metadata = sa.MetaData()
+    # destination_metadata.drop_all(destination_engine, checkfirst=False)
+
